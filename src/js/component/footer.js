4147faee3706c98a317dc91856f08087fc8dd2d4
Aleco112 <aleco112@yahoo.fr>
2022-04-19 00:58:56 +0000

    start

--- src/js/component/footer.js
+++ src/js/component/footer.js
@@ -2,9 +2,16 @@ import React, { Component } from "react";
 
 export const Footer = () => (
 	<footer className="footer mt-auto py-3 text-center">
-		<p>
-			Made with <i className="fa fa-heart text-danger" /> by{" "}
-			<a href="http://www.4geeksacademy.com">4Geeks Academy</a>
-		</p>
+			<div className="dropdown">
+  <button className="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false">
+    Create a new 
+  </button>
+  <ul className="dropdown-menu" aria-labelledby="dropdownMenuButton1">
+    <li><a className="dropdown-item" href="#">Action</a></li>
+    <li><a className="dropdown-item" href="#">Another action</a></li>
+    <li><a className="dropdown-item" href="#">Something else here</a></li>
+  </ul>
+</div>
+
 	</footer>
 );
---
+++ src/js/views/ContactListpage.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const ContactListpage = () => {
+  return (
+    <div>ContactListpage</div>
+  )
+}
