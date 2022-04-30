4b2e1da3bfbeda4b0a4dccc14ae6d44351c69b27
Aleco112 <aleco112@yahoo.fr>
2022-04-30 00:57:23 +0000

    new

--- src/js/component/footer.js
+++ src/js/component/footer.js
@@ -2,16 +2,6 @@ import React, { Component } from "react";
 
 export const Footer = () => (
 	<footer className="footer mt-auto py-3 text-center">
-			<div className="dropdown">
-  <button className="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false">
-    Create a new 
-  </button>
-  <ul className="dropdown-menu" aria-labelledby="dropdownMenuButton1">
-    <li><a className="dropdown-item" href="#">Action</a></li>
-    <li><a className="dropdown-item" href="#">Another action</a></li>
-    <li><a className="dropdown-item" href="#">Something else here</a></li>
-  </ul>
-</div>
-
+		
 	</footer>
 );
--- src/js/views/ContactListpage.js
+++ src/js/views/ContactListpage.js
@@ -2,6 +2,13 @@ import React from 'react'
 
 export const ContactListpage = () => {
   return (
-    <div>ContactListpage</div>
+    <div>
+      <div class="input-group mb-3">
+  <span class="input-group-text" id="inputGroup-sizing-default"></span>
+  <input type="text" class="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-default"/>
+</div>
+  
+
+    </div>
   )
 }
