92f7bd9cf6da67896bc2225e0747bbc9350e31d3
Aleco112 <aleco112@yahoo.fr>
2022-05-05 00:59:20 +0000

    contactpage

--- src/js/views/ContactListPage.js
+++ src/js/views/ContactListPage.js
@@ -1,16 +1,27 @@
-import React from "react";
+import React,{useContext}from "react";
 import ContactCard from "../component/ContactCard";
+import {Context} from "../store/appContext"
 
 export const ContactListPage = () => {
+  const { store , actions}=useContext(Context)
+
+  
+  console.log
   return (
     <div>
-      <ContactCard
-      image="https://st2.depositphotos.com/1531183/5770/v/950/depositphotos_57709697-stock-illustration-male-person-silhouette-profile-picture.jpg"
-      FullName="Ricardo Alexandre"
-      address="10745 N Kendall drive "
-      telephone=" 786-331-4678"
-      email="aleco112@yahoo.fr"
-      />
+      {store.contactList && store.contactList.map((contact,i)=>{
+        return(
+          <ContactCard
+          image="https://st2.depositphotos.com/1531183/5770/v/950/depositphotos_57709697-stock-illustration-male-person-silhouette-profile-picture.jpg"
+          FullName={contact.full_name}
+          address={contact.address}
+          telephone={contact.phone}
+          email= {contact.eamil}
+          key={i}
+          />
+          
+        )
+      })}
     </div>
   );
 };
